d29aeb661da6a55569ee8302bb1c9bb50b96c14b
李金文 <mihi@snapmail.cc>
2023-07-07 17:32:58 +0800

    添加下载文档

--- src/download/index.ts
+++ src/download/index.ts
@@ -3,7 +3,7 @@
  * @param {string} url
  * @return {*}
  */
- export const frontURLDownload = (url: string, name: string) => {
+ export const frontUrlDownload = (url: string, name: string) => {
   const a = document.createElement('a')
   a.href = url
   a.download = name
@@ -11,4 +11,45 @@
   document.body.appendChild(a)
   a.click()
   a.remove()
+}
+
+
+/**
+ * @description: 处理后端数据流下载 excel 表格, pdf
+ * @param {Blob} data
+ * @param {string} fileName
+ * @param {string} format
+ * @return {*}
+ */
+ export const dataStreamDownloadExcelOrPdf = (data: Blob, fileName: string, format: string, itype?: string) => {
+  let name!: string
+  let type = ''
+  if (format === 'pdf') {
+    type = itype ? itype : 'application/pdf;chartset=utf-8'
+    name = `${fileName}.pdf`
+  }
+  if (format === 'excel') {
+    type = itype ? itype : 'application/vnd.ms-excel;charset=utf-8'
+    name = `${fileName}.xlsx`
+  }
+  const blob = new Blob([data], { type: type })
+
+  if ('download' in document.createElement('a')) {
+    // 非IE下载
+    const elink = document.createElement('a')
+    elink.download = name
+    elink.style.display = 'none'
+    elink.href = URL.createObjectURL(blob)
+    document.body.appendChild(elink)
+    elink.click()
+    URL.revokeObjectURL(elink.href)
+    document.body.removeChild(elink)
+  } else {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ;(window.navigator as any).msSaveBlob(blob, name)
+    } catch (e) {
+      console.log(e)
+    }
+  }
 }
