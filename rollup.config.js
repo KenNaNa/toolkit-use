2a1594daa974badf46f282b71a8d5549dadb7c42
李金文 <mihi@snapmail.cc>
2023-07-11 11:11:39 +0800

    优化 rollup.config.js 打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -31,6 +31,7 @@ const banner =
 const filePathArr = []
 const filenameArr = []
 let rollupOptions = []
+// 构建 exports 配置项
 const buildRollupOptions =  (filePathArr) => {
   const rollupOptions = []
   const plugins = [
@@ -50,481 +51,56 @@ const buildRollupOptions =  (filePathArr) => {
   ]
   filePathArr.forEach((item) => {
     rollupOptions.push({
-      input: resolvePath(item),
+      input: resolvePath(`.${item}`),
       output: {
-        file: item.replace('src', 'dist').replace('index.ts', `index.esm.js`),
+        file: item.replace('/src', 'dist').replace('index.ts', `index.esm.js`),
         format: 'esm',
         banner,
+        name: `${item}`,
       },
       plugins,
     })
     rollupOptions.push({
-      input: resolvePath(item),
+      input: resolvePath(`.${item}`),
       output: {
-        file: item.replace('src', 'dist').replace('index.ts', `index.umd.js`),
+        file: item.replace('/src', 'dist').replace('index.ts', `index.umd.js`),
         format: 'umd',
         banner,
+        name: `${item}`,
       },
       plugins,
     })
   })
   return rollupOptions
 }
+// 读取文件
 const fileDisplay = (filePath) => {
-  return new Promise((resolve) => {
-    fs.readdirSync(filePath, (err, files) => {
-      if (err) return console.error('Error:(spec)', err)
-      files.forEach((filename) => {
-        //获取当前文件的绝对路径
-        if(filename !== 'index.ts') {
-          filenameArr.push(filename)
-        } else {
-          filenameArr.push(null)
-        }
-        const filedir = path.join(filePath, filename)
-        // fs.stat(path)执行后，会将stats类的实例返回给其回调函数。
-        fs.statSync(filedir, (eror, stats) => {
-          if (eror) return console.error('Error:(spec)', err)
-          // 是否是文件
-          const isFile = stats.isFile()
-          // 是否是文件夹
-          const isDir = stats.isDirectory()
-          if (isFile) {
-            // 这块我自己处理了多余的绝对路径，第一个 replace 是替换掉那个路径，第二个是所有满足\\的直接替换掉
-            filePathArr.push(filedir.replace(__dirname, '').replace(/\\/img, '/'))
-            // 最后打印的就是完整的文件路径了
-            console.log(filePathArr, 'arr', filenameArr)
-            rollupOptions = buildRollupOptions(filePathArr)
-            resolve(rollupOptions)
-          }
-          // 如果是文件夹
-          if (isDir) fileDisplay(filedir);
-        })
-      });
-    });
-  })
+  readdirSync = fs.readdirSync(filePath)
+  readdirSync.forEach((filename) => {
+    //获取当前文件的绝对路径
+    if(filename !== 'index.ts') {
+      filenameArr.push(filename)
+    }
+    const filedir = path.join(filePath, filename)
+    const stats = fs.statSync(filedir)
+    // 是否是文件
+    const isFile = stats.isFile()
+    // 是否是文件夹
+    const isDir = stats.isDirectory()
+    if (isFile) {
+      // 这块我自己处理了多余的绝对路径，第一个 replace 是替换掉那个路径，第二个是所有满足\\的直接替换掉
+      filePathArr.push(filedir.replace(__dirname, '').replace(/\\/img, '/'))
+    }
+    // 如果是文件夹
+    if (isDir) fileDisplay(filedir);
+  });
 }
 
-fileDisplay(filePath).then((res) => {
-  console.log('res====>', res)
-  module.exports = res
-})
+const getOptions =  () => {
+  fileDisplay(filePath)
+  // 最后打印的就是完整的文件路径了
+  rollupOptions = buildRollupOptions(filePathArr)
+  return rollupOptions
+}
 
-
-
-module.exports = [
-  {
-    input: resolvePath('src/index.ts'),
-    output: {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-      name: 'index',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/index.ts'),
-    output: {
-      file: 'dist/index.umd.js',
-      format: 'umd',
-      name: 'index',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/array/index.ts'),
-    output: {
-      file: 'dist/array/index.esm.js',
-      format: 'esm',
-      name: 'array',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/array/index.ts'),
-    output: {
-      file: 'dist/array/index.umd.js',
-      format: 'umd',
-      name: 'array',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/clip/index.ts'),
-    output: {
-      file: 'dist/clip/index.esm.js',
-      format: 'esm',
-      name: 'clip',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/clip/index.ts'),
-    output: {
-      file: 'dist/clip/index.umd.js',
-      format: 'umd',
-      name: 'clip',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/decimal/index.ts'),
-    output: {
-      file: 'dist/decimal/index.esm.js',
-      format: 'esm',
-      name: 'decimal',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/decimal/index.ts'),
-    output: {
-      file: 'dist/decimal/index.umd.js',
-      format: 'umd',
-      name: 'decimal',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/download/index.ts'),
-    output: {
-      file: 'dist/download/index.esm.js',
-      format: 'esm',
-      name: 'download',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/download/index.ts'),
-    output: {
-      file: 'dist/download/index.umd.js',
-      format: 'umd',
-      name: 'download',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/string/index.ts'),
-    output: {
-      file: 'dist/string/index.esm.js',
-      format: 'esm',
-      name: 'string',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/string/index.ts'),
-    output: {
-      file: 'dist/string/index.umd.js',
-      format: 'umd',
-      name: 'string',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/date/index.ts'),
-    output: {
-      file: 'dist/date/index.esm.js',
-      format: 'esm',
-      name: 'date',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/date/index.ts'),
-    output: {
-      file: 'dist/date/index.umd.js',
-      format: 'umd',
-      name: 'date',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/event-hub/index.ts'),
-    output: {
-      file: 'dist/event-hub/index.esm.js',
-      format: 'esm',
-      name: 'event-hub',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/event-hub/index.ts'),
-    output: {
-      file: 'dist/event-hub/index.umd.js',
-      format: 'umd',
-      name: 'event-hub',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/regex/index.ts'),
-    output: {
-      file: 'dist/regex/index.esm.js',
-      format: 'esm',
-      name: 'regex',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-  {
-    input: resolvePath('src/regex/index.ts'),
-    output: {
-      file: 'dist/regex/index.umd.js',
-      format: 'umd',
-      name: 'regex',
-    },
-    plugins: [
-      nodeResolve({
-        extensions,
-        modulesOnly: true
-      }),
-      commonjs(),
-      typescript(),
-      babel({
-        babelHelpers: 'runtime',
-        include: 'src/**',
-        exclude: 'node_modules/**',
-        extensions
-      }),
-      terser()
-    ]
-  },
-]
+module.exports = getOptions()
